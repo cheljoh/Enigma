02e0176091e8092d3e9d4ca8b35d61d9534e4556
Chelsea Johnson <cheljoh@gmail.com>
2015-12-16 13:45:02 -0700

    added travis

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -1,40 +1,41 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-#require '../lib/cracker'
+require '../lib/cracker'
 
 class CrackTest < Minitest::Test
 
   def test_does_it_order_numbers_into_correct_position
-    skip
-    crack = Cracker.new("2yc69s2uz76uu")
+    known_phrase = "nd.."
+    output = "2yc69s2uz76uu"
+    crack = Cracker.new
     cipher = Cipher.new
-    character_list = cipher.characters_to_numbers(output.strip)
-    last_four_unknown = character_list[-4..-1] #what are the letters
-    last_four_known = [13, 3, 37, 37]
-    order_list(character_list, last_four_known, last_four_unknown)
-    assert_equal [37, 13, 3, 37], crack.known
-    assert_equal [20, 33, 32, 20], crack.unknown
+    last_four_unknown = cipher.characters_to_numbers(output[-4..-1])
+    last_four_known = cipher.characters_to_numbers(known_phrase[-4..-1])
+    known = crack.order_list(output, last_four_known)
+    unknown = crack.order_list(output, last_four_unknown)
+    assert_equal [37, 13, 3, 37], crack.order_list(output, last_four_known)
+    assert_equal [20, 33, 32, 20], crack.order_list(output, last_four_unknown)
   end
 
   def test_does_it_calculate_key
-    skip
-    crack = Cracker.new("2yc69s2uz76uu")
+    crack = Cracker.new
+    output = "2yc69s2uz76uu"
     cipher = Cipher.new
-    character_list = cipher.characters_to_numbers(output.strip)
-    last_four_unknown = character_list[-4..-1] #what are the letters
-    last_four_known = [13, 3, 37, 37]
-    order_list(character_list, last_four_known, last_four_unknown)
-    date_calculation
-    init_date_offsets
-    key_calculation
-    assert_equal 15654, crack.key
+    known_phrase = "nd.."
+    last_four_unknown = cipher.characters_to_numbers(output[-4..-1])
+    last_four_known = cipher.characters_to_numbers(known_phrase[-4..-1])
+    known = crack.order_list(output, last_four_known)
+    unknown = crack.order_list(output, last_four_unknown)
+    crack.init_date_offsets
+    base_key = crack.get_base_key(known, unknown)
+    candidates = crack.key_candidates(base_key)
+    assert_equal "15654", crack.test_keys(output, known_phrase, candidates)
   end
 
   def test_does_it_crack_the_code
-    skip
-    crack = Cracker.new("2yc69s2uz76uu", Time.now)
-    assert_equal "hello ..end..", crack.crack
+    crack = Cracker.new(Time.now)
+    assert_equal "hello ..end..", crack.crack("2yc69s2uz76uu")
   end
 
 end
